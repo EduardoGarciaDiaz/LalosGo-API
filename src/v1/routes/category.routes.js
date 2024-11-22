40436abb23ff7ac4e9ca962335a812ddacade0e6
TristanSantiago1 <tristan.santiago520@gmail.com>
2024-11-22 11:51:19 -0600

    Fix method router() in Category.index

--- src/v1/routes/category.routes.js
+++ src/v1/routes/category.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const CategoriesController = require('../../controllers/categories.controller.js');
 
 router.post('/', CategoriesController.createCategory);
