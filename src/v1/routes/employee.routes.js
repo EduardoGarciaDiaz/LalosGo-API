9d31c4082f537de564667e91fd2cdb82bb44e1d4
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2025-01-06 23:34:53 -0600

    Refactor employee roles and update authorization for employee and order routes

--- src/v1/routes/employee.routes.js
+++ src/v1/routes/employee.routes.js
@@ -12,6 +12,6 @@ router.put('/:id', authorize('Administrator'), employees.update);
 
 router.patch('/:id', authorize('Administrator'), employees.updateStatus);
 
-router.get('/:role/branch/:branchId', authorize('Administrator, Manager,Sales Executive'), employees.getEmployeeByRole);
+router.get('/:role/branch/:branchId', authorize('Administrator,Sales Executive'), employees.getEmployeeByRole);
 
 module.exports = router;
--- src/v1/routes/order.routes.js
+++ src/v1/routes/order.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const authorize = require('../../middlewares/auth.middleware');
 const orders = require('../../controllers/orders.controller.js');
 
-router.get('/', authorize('Manager,Customer,Delivery Person,Sales Executive'), orders.getAll);
-router.get('/:orderId', authorize('Manager,Customer,Delivery Person,Sales Executive'), orders.get);
+router.get('/', authorize('Customer,Delivery Person,Sales Executive'), orders.getAll);
+router.get('/:orderId', authorize('Customer,Delivery Person,Sales Executive'), orders.get);
 router.put('/:orderId', authorize('Customer'), orders.cartToOrder);
 router.put('/:orderId/deliveryPerson/:deliveryPersonId', authorize('Sales Executive'), orders.assignDeliveryPerson);
 router.post('/:id/:status', authorize('Sales Executive,Delivery Person,Customer'), orders.updateStatus);
